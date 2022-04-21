466a0ef5b9eb2b87968599270df88f9b81a2299e
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-04-21 10:57:11 -0700

    set up eslint

--- client/lib/hash-router.js
+++ client/lib/hash-router.js
@@ -1,52 +1,53 @@
-import React from 'react'
+import React from 'react';
 
 export function useHashRouter() {
-  const router = React.useContext(HashRouterContext)
-  return router
+  const router = React.useContext(HashRouterContext);
+  return router;
 }
 
 export function Redirect(props) {
-  const router = useHashRouter()
-  useEffect(() => router.redirect(props.to), [])
-  return null
+  const router = useHashRouter();
+  React.useEffect(() => router.redirect(props.to), []);
+  return null;
 }
 
 export function HashRouter({ children }) {
 
-  const [hash, setHash] = React.useState(window.location.hash)
+  const [hash, setHash] = React.useState(window.location.hash);
 
   const router = React.useMemo(() => {
-    const hashRoute = hash.replace(/^#/, '')
-    const [path, search = ''] = hashRoute.split('?')
-    const params = new URLSearchParams(search)
-    return { path, params, redirect, navigate }
-  }, [hash])
+    const hashRoute = hash.replace(/^#/, '');
+    const [path, search = ''] = hashRoute.split('?');
+    const params = new URLSearchParams(search);
+    return { path, params, redirect, navigate };
+  }, [hash]);
 
   React.useEffect(() => {
-    const handleHashchange = () => setHash(() => window.location.hash)
-    window.addEventListener('hashchange', handleHashchange)
-    return () => window.removeEventListener('hashchange', handleHashchange)
-  }, [])
+    const handleHashchange = () => setHash(() => window.location.hash);
+    window.addEventListener('hashchange', handleHashchange);
+    return () => window.removeEventListener('hashchange', handleHashchange);
+  }, []);
 
-  return React.createElement(HashRouterContext.Provider, {
-    value: router,
+  return React.createElement(
+    HashRouterContext.Provider,
+    { value: router },
     children
-  })
+  );
 }
 
 const redirect = hash => {
-  const url = new URL(window.location.href)
-  url.hash = hash
-  window.location.replace(url)
-}
+  const url = new URL(window.location.href);
+  url.hash = hash;
+  window.location.replace(url);
+};
 
 const navigate = hash => {
-  window.location.hash = hash
-}
+  window.location.hash = hash;
+};
 
 const HashRouterContext = React.createContext({
   path: '',
   params: new URLSearchParams(),
   navigate: hash => {},
   redirect: hash => {}
-})
+});
